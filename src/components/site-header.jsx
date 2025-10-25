e659953841fda2810c7140794dad5e5737871973
Ali-Ishaq <aliishaq3578@gmail.com>
2025-10-26 02:50:20 +0500

    feat: consistent gpa calculator data & ui enhancements

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -40,7 +40,7 @@ export function SiteHeader() {
         />
         <h1 className="text-base font-medium">{title}</h1>
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+          {/* <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
               href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
               rel="noopener noreferrer"
@@ -48,7 +48,7 @@ export function SiteHeader() {
               className="dark:text-foreground">
               GitHub
             </a>
-          </Button>
+          </Button> */}
         </div>
       </div>
     </header>
